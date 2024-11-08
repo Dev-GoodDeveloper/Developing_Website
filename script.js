f776603026249c8b0f309e8c8583ccca3d624024
Dev-GoodDeveloper <dev.gooddeveloper@gmail.com>
2024-11-08 10:08:04 +0530

    Added ALL

---
+++ script.js
@@ -0,0 +1,10 @@
+// Toggle menu on mobile
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+// Alert message for "Learn More" button
+function showAlert() {
+    alert("Thank you for your interest! More details coming soon.");
+}
